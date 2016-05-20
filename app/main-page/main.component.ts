4699d06a2e367d3f4b785882de0511c87642d16f
ravkovskiy <kos1992@mail.ru>
2016-05-20 09:08:08 +0300

    del app folder

--- app/main-page/main.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "angular2/core";
-import {CarouselComponent} from "../carousel/carousel.component";
-@Component({
-    selector: 'main_component',
-    directives: [CarouselComponent],
-    templateUrl: "app/main-page/main.template.html",
-})
-export class MainComponent{}
